48cb8f5b1e7ea9682d047fdb27288bb6a7d4bca7
romeobarrientos <rgbarrie@asu.edu>
2022-08-13 21:24:50 -0700

    Created javaTypes
    A program was created that uses illegal field names and checks compilation error procedures.

---
+++ javaTypes.java
@@ -0,0 +1,22 @@
+
+public class javaTypes {
+
+public static void main(String[] args){
+           // Initialize illegal field names
+           int num = 12;
+           String cow = "Moo!";
+           boolean smart = false;
+           double cost = 24.50;
+           float price = 'F';
+           long value = 2334;
+           char grade = 2.0f;
+           char grade2;
+           System.out.println("The cow says " + cow + " " + num + "times!");
+           System.out.println("The price of the cow is " + cost + " but the actual price is $" + price + ".");
+           System.out.println("This value is " + grade2);
+           System.out.println("Your final grade is " + grade + " you are not " + smart);
+    }
+
+    
+
+}
