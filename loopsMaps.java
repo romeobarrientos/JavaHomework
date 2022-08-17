1217d9fe3ab8a5ff0500388e46c1e80b3beaa80f
romeobarrientos <rgbarrie@asu.edu>
2022-08-16 18:04:32 -0700

    Corrected smallest value not being output to console
    There was an issue where the smallest element was continuously returned as zero. This was happening because the list was not populated yet when the value smallest was called.

--- loopsMaps.java
+++ loopsMaps.java
@@ -10,34 +10,40 @@ public class loopsMaps{
         try (Scanner scan = new Scanner(System.in)) {
          System.out.println("Enter 5 numbers please!");
 
-            //After sorting array, initialize highest and lowest value
-            int largest = list[0];
-            int smallest = list[0];
+            
+            
 
             //Apply values to sum and product
             for(int i = 0; i < list.length; i++){
                 list[i] = scan.nextInt();
                 sum += list[i];
                 product *= list[i];
+            }
 
-                //Sort array to find smallest and largest values.
+            //Highest and lowest value used in if statement below
+            int largest = list[0];
+            int smallest = list[0];
+
+            for(int i = 0; i < list.length; i++){
+                 //Sort array to find smallest and largest values.
+                
                 if (list[i] > largest)
-                    largest = list[i];
-                else if (list[i] < smallest)
-                    smallest = list[i];
-    }
+                largest = list[i];
+            if (list[i] < smallest)
+                smallest = list[i];
 
-            //Output results
+            }
+                //Output results
             System.out.println("The sum of the array is " + sum);
             System.out.println("The product of the array is " + product);
             System.out.println("The largest element in the array is " + largest);
             System.out.println("The smallest element in the array is " + smallest);
+               
+    }
+
+           
             }
             
         }
-        
-
-       
-    }
 
     
